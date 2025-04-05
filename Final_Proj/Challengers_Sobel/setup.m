84603739574e22c12bc5d7ea9ebd11f7252e10cc
obb2733 <144734325+obb2733@users.noreply.github.com>
2025-04-05 18:14:51 -0400

    Vertical and SQRT Functionality Added

--- Final_Proj/Challengers_Sobel/setup.m
+++ Final_Proj/Challengers_Sobel/setup.m
@@ -4,6 +4,9 @@
 coefficients_sobel = [1 0 -1;
                       2 0 -2;
                       1 0 -1];
+coefficients_sobel_vert = [1 2 1;
+                           0 0 0;
+                           -1 -2 -2];
 coefficients_gaussian = [1/16 1/8 1/16;
                          1/8 1/4 1/8;
                          1/16 1/8 1/16];
